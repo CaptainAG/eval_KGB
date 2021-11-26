b5b9112daa7cb572d170d182eaa1d5b7d1b08122
CaptainAG <85942725+CaptainAG@users.noreply.github.com>
2021-11-26 14:36:13 +0100

    Mise en place du fichier index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Mission </title>
+</head>
+<body>
+    <main>
+        <div class="container-xx">
+            <div class="row justify-content-center text-center">
+                <h1>Missions</h1>
+                <table class="table table-striped table-hover table-responsive text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">Titre</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Code</th>
+                            <th scope="col">Pays</th>
+                            <th scope="col">Date de début</th>
+                            <th scope="col">Date de fin </th>
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+		            <tbody>
+                        <tr>
+                            <td scope="row"></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td> <a href="">show</a> </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </main>
+    
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+</html>
